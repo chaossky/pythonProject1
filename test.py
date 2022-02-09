cbabd90aea3e47f0e417ad71ed357052e436f580
이장훈 <chaossky@naver.com>
2022-02-09 17:36:29 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,48 @@
+import time
+import pyautogui
+import pyperclip
+
+# data = open('test_sample.txt', 'r', encoding='utf8')
+# data = open('intro.txt', 'r', encoding='utf8')
+data = open('contents001.txt', 'r', encoding='utf8')
+# data = open('article001.txt', 'r', encoding='utf8')
+count = 0
+lines = data.read()
+data.close()
+
+str001 = lines.split(" ")
+
+str_list = (list(str001))
+
+#print(str_list)
+
+time.sleep(3)
+
+for i in range(len(str_list)):
+    time.sleep(0.1)
+    if str_list[i].count('\\n') > 0:
+        pyautogui.hotkey('enter')
+        pyperclip.copy(str_list[i])
+        pyautogui.hotkey('ctrl', 'v')
+    # if str_list[i] == '\\' and str_list[i + 1] == "n":
+    #     pyautogui.hotkey('enter')
+    #     pyperclip.copy(str_list[i])
+    #     # pyautogui.hotkey('space')
+    #     pyautogui.hotkey('ctrl', 'v')
+    # if str_list[i - 1] == '\\' and str_list[i] == 'n':
+    #     pass
+    else:
+        pyperclip.copy(str_list[i])
+        pyautogui.hotkey('space')
+        pyautogui.hotkey('ctrl', 'v')
+
+# for i in range(len(str_list)):
+#     time.sleep(0.1)
+#     if "." in str_list[i]:
+#         pyperclip.copy(str_list[i])
+#         pyautogui.hotkey('ctrl', 'v')
+#         pyautogui.hotkey('enter')
+#     else:
+#         pyperclip.copy(str_list[i])
+#         pyautogui.hotkey('space')
+#         pyautogui.hotkey('ctrl', 'v')
